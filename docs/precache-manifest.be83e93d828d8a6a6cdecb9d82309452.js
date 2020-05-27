e33ae8a9f44401268a14ba8879d172e72821bcbf
Herpiko Dwi Aguno <herpiko@gmail.com>
2020-05-27 08:14:12 +0700

    First build.

---
+++ docs/precache-manifest.be83e93d828d8a6a6cdecb9d82309452.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9f98e56dfd83951a1cc8fe74b76e915",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6421661249afdc5fdb9c",
+    "url": "/static/css/main.53c30cbc.chunk.css"
+  },
+  {
+    "revision": "3fc32632b2298726ee89",
+    "url": "/static/js/2.3317d699.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3317d699.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6421661249afdc5fdb9c",
+    "url": "/static/js/main.c14126a8.chunk.js"
+  },
+  {
+    "revision": "0d9433d91016348884b0",
+    "url": "/static/js/runtime-main.ffdd516e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
